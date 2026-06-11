036202475b022986135cc7d5334a1ae41a1d058b
Franca-Dev25 <agbenyenufrancisca66@gmail.com>
2026-06-11 12:41:10 +0000

    Add files via upload

---
+++ Tables and chairs/src/work2.java
@@ -0,0 +1,10 @@
+public class work2 {
+    public static void main(String[] args){
+        System.out.println("X                          X");
+        System.out.println("X                          X");
+        System.out.println("X       XXXXXXXXXX         X");
+        System.out.println("XXXXXX  x        x    XXXXXX");
+        System.out.println("X    X  X        X    X    X");
+        System.out.println("X    X  X        X    X    X");
+    }
+}
